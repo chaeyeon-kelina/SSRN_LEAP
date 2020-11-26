b52aa338b1d69f6d6a69614d652b9b0abc24bc8d
kkunmando <69005820+kkunmando@users.noreply.github.com>
2020-11-26 15:45:25 +0900

    자주가는 생협 등록까지 구현

--- app/src/main/java/com/example/ssrn/MainActivity.java
+++ app/src/main/java/com/example/ssrn/MainActivity.java
@@ -19,16 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn_login = findViewById(R.id.btn_login);
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //login 으로 가는 인텐트 생성
-                Intent intent = new Intent(MainActivity.this, login.class);
-                startActivity(intent);
-            }
-        });
-
+        //텍스트 화면에 출력
         TextView text_check = (TextView) findViewById(R.id.text_check);
         text_check.setText("자동로그인");
 
@@ -40,6 +31,26 @@ public class MainActivity extends AppCompatActivity {
 
         EditText login_pw = (EditText) findViewById(R.id.login_pw);
         login_pw.setHint("비밀번호");
+
+        //버튼 연결
+        text_find_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //login 으로 가는 인텐트 생성
+                Intent intent = new Intent(MainActivity.this, find_id.class);
+                startActivity(intent);
+            }
+        });
+
+        Button b = (Button) findViewById(R.id.btn_login);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //login 으로 가는 인텐트 생성
+                Intent intent = new Intent(MainActivity.this, Register_favorite_store.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
