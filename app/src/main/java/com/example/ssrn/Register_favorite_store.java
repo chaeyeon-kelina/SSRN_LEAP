6fe4e4dcb33f339a48458b552b766b0081352bc7
kkunmando <69005820+kkunmando@users.noreply.github.com>
2020-11-26 16:38:25 +0900

    11/26
    11/26

---
+++ app/src/main/java/com/example/ssrn/Register_favorite_store.java
@@ -0,0 +1,48 @@
+package com.example.ssrn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Register_favorite_store extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_favorite_store);
+
+        //텍스트 화면에 출력
+        TextView title_bar = (TextView)findViewById(R.id.title_bar);
+        title_bar.setText("자주 가는 생협 등록");
+
+        TextView store_text1 = (TextView) findViewById(R.id.store_text1);
+        store_text1.setText("학문관");
+        TextView store_text2 = (TextView) findViewById(R.id.store_text2);
+        store_text2.setText("ECC 기념품점");
+        TextView store_text3 = (TextView) findViewById(R.id.store_text3);
+        store_text3.setText("학문관 제과점");
+        TextView store_text4 = (TextView) findViewById(R.id.store_text4);
+        store_text4.setText("교육관");
+        TextView store_text5 = (TextView) findViewById(R.id.store_text5);
+        store_text5.setText("산학협력관");
+        TextView store_text6 = (TextView) findViewById(R.id.store_text6);
+        store_text6.setText("공학관");
+        TextView store_text7 = (TextView) findViewById(R.id.store_text7);
+        store_text7.setText("과학관");
+        TextView store_text8 = (TextView) findViewById(R.id.store_text8);
+        store_text8.setText("법학관");
+
+
+
+
+    }
+
+}
